c5a2bbc886a71f12bf0178f98f9e7a6572331f50
grzeg <grzegorzg45@gmail.com>
2023-07-06 14:17:23 +0200

    added finished recruitment task

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,44 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    public static void main(String[] args)
+    {
+
+        int [] quantity = {1, 3, 5, 10, 20, 200, 100, 100, 10000};
+
+        whatChange(1.30, quantity);
+        whatChange(11.70, quantity);
+        whatChange(6.70, quantity);
+        whatChange(4.30, quantity);
+
     }
+
+    public static void whatChange(double change, int [] quantity) {
+        System.out.println("Dla reszty " + change + " zł:");
+
+        int changeGr = (int) (change * 100);
+        int[] nominal = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+
+
+        for (int i = 0; i < nominal.length; i++)
+        {
+            int value = changeGr / nominal[i];
+
+            for (int v = value; v>0 ;v--)
+               if (v <= quantity[i])
+            {
+                System.out.println("Wydaj " + v + " monet " + (nominal[i] / 100.0) + " zł");
+                changeGr -= v * nominal[i];
+                quantity[i] -= v;
+                break;
+            }
+        }
+
+        System.out.println();
+    }
+
+
+
+
 }
